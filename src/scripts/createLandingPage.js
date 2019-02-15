bfd34c7e66c4f576feca5f74de3a77a42b2d8018
Abbey Brown <abbeybrownsemail@gmail.com>
2019-02-15 13:04:08 -0600

    wrote functions for landing page

---
+++ src/scripts/createLandingPage.js
@@ -0,0 +1,38 @@
+import createLoginForm from "./createLoginForm"
+/*
+    Author: Abbey
+    Name: createLandingPage.js
+    Purpose: landing page that allows the user to choose between registering as a new member or logging in
+*/
+
+
+import createRegistrationForm from "./createRegistrationForm"
+
+const createLandingPage = () => {
+    document.querySelector(".output").innerHTML += `
+        <h1>Welcome to Humming Hazelnuts Dashboard Page </h1>
+        <div class="buttonSection">
+        <button id="loginButton">Login</button>
+        <button id="registerButton">Register</button>
+        </div>
+    `
+}
+
+
+const changePage = {
+
+    listenForEvent: () => {
+
+        document.querySelector(".output").addEventListener("click", () => { 
+            if (event.target.id.startsWith("loginButton")) {
+                createLoginForm() //should the submit Login.js just be a function here??
+            } else if (event.target.id.startsWith("registerButton")) {
+                createRegistrationForm()
+            }
+        })
+
+    }
+}
+
+
+export { createLandingPage, changePage }
