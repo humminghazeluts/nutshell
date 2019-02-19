1243cb012efd3de19236e7f48ebf75ff78e3dafc
nathan sepulveda <nathans.composer@gmail.com>
2019-02-19 15:23:09 -0600

    user's can now not edit another user's message

--- src/scripts/editMessage.js
+++ src/scripts/editMessage.js
@@ -4,18 +4,27 @@ import buttonText from "./messageButtonStatus"
 let editMessage = () => {
     let messageDisplay = document.querySelector("#messageList")
     let messagedId = document.getElementById("messageId")
-    
+    let userId = Number(sessionStorage.getItem("userId"))
 
     messageDisplay.addEventListener("click", event => {
         // buttonText()
+        
         if (event.target.id.startsWith("--")) {
             console.log("edit clicked")
+            
             let messageId = event.target.id.split("--")[1]
             console.log(messageId)
             APIManager.getindividualMessages(messageId).then(response => {
+                console.log(userId)
+                console.log(response.userId)
+                if (userId === response.userId) {
                 console.log(response)
                 document.getElementById("messageText").value = response.messages;
                 document.getElementById("messageId").value = response.id;
+                } 
+                else {
+                    alert("You can't edit another user's messages!")
+                }
             })
         }
     })
