07698ed9571d9a2ad87cbd4c3fe346f1dc39a205
Trevan Young <trevan.young@gmail.com>
2019-02-20 10:34:34 -0600

    Edit and Modify Work Now

--- src/scripts/addTaskForm.js
+++ src/scripts/addTaskForm.js
@@ -1,11 +1,14 @@
+import APIManager from "./dataManager"
 import postNewTask from "./submitAddTask"
+import createTask from "./addTaskObjectCreate"
+import printTasksToDom from "./taskPrintToDom"
 
 const addTask = () => {
     document.querySelector("#taskFormOutput").innerHTML =
      `
         <div id="taskFormOutput">
 
-            <input type="hidden" id="userID">
+            <input type="hidden" id="hiddenTaskInput">
 
         <fieldset>
             <label for="task"></label>
@@ -26,9 +29,24 @@ const addTask = () => {
         <button id="addTaskButton">Add Task!</button>
         </div>
         `
-
+        //Will Post New Obect if button says task
         document.querySelector("#addTaskButton").addEventListener("click", (event) => {
-            postNewTask()
+            if(event.target.textContent === "Add Task!"){
+                postNewTask()
+            }
+            //Will edit selected task, and then
+            else if (event.target.textContent === "Update Task"){
+                const taskId = document.querySelector("#hiddenTaskInput").value
+                const taskName = document.querySelector("#taskName__input").value
+                const taskDate = document.querySelector("#taskDate__input").value
+                const completed= document.querySelector("#completed__input").checked
+                const newTaskObject = createTask(taskName, taskDate, completed)
+                event.target.textContent = "Add Task!"
+                APIManager.editTask(Number(taskId), newTaskObject).then(()=>{
+                printTasksToDom()
+
+                })
+            }
     } )
 
 
--- src/scripts/submitAddTask.js
+++ src/scripts/submitAddTask.js
@@ -6,17 +6,12 @@ import printTasksToDom from "./taskPrintToDom"
 
     let postNewTask = () => {
     // Input is gathered from the selected IDs and stored in a variable
+    // Input fields are queried and based on ID
     const taskName = document.querySelector("#taskName__input").value
     const taskDate = document.querySelector("#taskDate__input").value
     const completed= document.querySelector("#completed__input").checked
-
     const newTaskObject = createTask(taskName, taskDate, completed)
     APIManager.postNewTask(newTaskObject)
-
-
-
-    // apiManager.postSubmit(newTaskObject)
-
 }
 
 
